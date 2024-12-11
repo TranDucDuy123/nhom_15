9489726ca9e9470aaf608019ebf04d5b4cee802b
duy1233 <dyly399@gmail.com>
2024-12-11 07:38:16 +0700

    fix

--- MyAspNetApp/obj/Debug/net8.0/MyAspNetApp.AssemblyInfo.cs
+++ MyAspNetApp/obj/Debug/net8.0/MyAspNetApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAspNetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a586616113c91b74fd71cb4ead173e89765190")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAspNetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAspNetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
